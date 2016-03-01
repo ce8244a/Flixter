0613d2b49c109b7825ff338ad4d54f2f160d610c
Christine <cmelasigue@gmail.com>
2016-03-01 04:14:05 +0000

    Lesson 31 Reorder Sections

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:new, :create]
+  #before_action :require_authorized_for_current_section, only: [:new, :create]
   before_action :require_authorized_for_current_lesson, only: [:update]
 
 	def new
@@ -21,7 +21,7 @@ class Instructor::LessonsController < ApplicationController
 
   def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
-      render text: 'Unauthorized', status: :unauthorized
+      return render :text => 'Unauthorized', status: :unauthorized
     end
   end
 
@@ -29,9 +29,17 @@ class Instructor::LessonsController < ApplicationController
     @current_lesson ||= Lesson.find(params[:id])
   end
 
+  def lesson_params
+    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
+  end
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      return render text: 'Unauthorized', status: :unauthorized
+      return render :text => 'Unauthorized', status: :unauthorized
     end
   end
 
@@ -40,7 +48,4 @@ class Instructor::LessonsController < ApplicationController
     @current_section ||= Section.find(params[:section_id])
   end
 
-  def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
-  end
 end
